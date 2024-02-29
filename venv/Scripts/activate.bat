6ad9f2d3bee586470471a3af5438cd80df121753
Sharif Orzikulov <orzikulov@gmail.com>
2024-02-29 12:43:03 +0300

    Environment changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\x240\Downloads\VSCode\Human-Resources-Management-System\Human-Resources-Management-System\venv
+set VIRTUAL_ENV=C:\Users\x240\Downloads\VSCode\HRM-system\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
