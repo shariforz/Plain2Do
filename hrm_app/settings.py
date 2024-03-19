f28ab950eb7019ba99b306dbf4e5bc798da9b6fa
Sharif Orzikulov <orzikulov@gmail.com>
2024-03-19 23:26:32 +0300

    Settings changed

--- hrm_app/settings.py
+++ hrm_app/settings.py
@@ -13,13 +13,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from django.utils.translation import gettext_lazy as _
 
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
