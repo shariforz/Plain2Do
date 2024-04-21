e3f93e0bc03cc38336ac4de86b8802764c645eab
Qobi1 <tolaganov03@list.ru>
2024-04-21 13:35:03 +0500

    sqlite db

--- hrm_app/settings.py
+++ hrm_app/settings.py
@@ -16,9 +16,9 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-from dotenv import load_dotenv
-
-load_dotenv()
+# from dotenv import load_dotenv
+#
+# load_dotenv()
 #
 DEBUG = True
 
